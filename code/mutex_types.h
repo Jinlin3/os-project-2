8b59ffb73294a5bf00bd5dc31bb9b48e360d0a91
Jack Lin <hellojjlin@gmail.com>
2024-02-18 08:18:03 -0500

    init

---
+++ code/mutex_types.h
@@ -0,0 +1,16 @@
+#ifndef MTX_TYPES_H
+#define MTX_TYPES_H
+
+// contains definitions for data structures needed for implementing threads
+
+#include "thread_worker_types.h"
+
+/* mutex struct definition */
+typedef struct worker_mutex_t
+{
+    /* add something here */
+
+    // YOUR CODE HERE
+} worker_mutex_t;
+
+#endif
---
+++ code/thread_worker_types.h
@@ -0,0 +1,24 @@
+#ifndef TW_TYPES_H
+#define TW_TYPES_H
+
+// contains definitions for data structures needed for implementing threads
+
+#include <ucontext.h>
+
+typedef unsigned int worker_t;
+
+typedef struct TCB
+{
+    /* add important states in a thread control block */
+    // thread Id
+    // thread status
+    // thread context
+    // thread stack
+    // thread priority
+    // And more ...
+
+    // YOUR CODE HERE
+
+} tcb;
+
+#endif
