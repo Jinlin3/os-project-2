7728652c7395c576df83430b21b96ef31a626a9e
Jack Lin <hellojjlin@gmail.com>
2024-02-22 03:08:54 -0500

    finished foobar program, understand timers

--- sample-code/foobar.c
+++ sample-code/foobar.c
@@ -4,36 +4,38 @@
 #include <sys/time.h>
 #include <string.h>
 #include <ucontext.h>
+#include <unistd.h>
 
 #define STACK_SIZE 16 * 1024
 
-ucontext_t fooctx, barctx, scheduler;
-int flipflop = 0;
+ucontext_t fooctx, barctx, scheduler, flipctx;
+int flipflop = 1;
 
 void foo() {
   // setting up timer
-  struct sigaction sa;
-	memset (&sa, 0, sizeof(sa));
-	sa.sa_handler = &bar;
-	sigaction (SIGPROF, &sa, NULL);
-
-	struct itimerval timer;
-	timer.it_interval.tv_usec = 0;
-	timer.it_interval.tv_sec = 1;
-	timer.it_value.tv_usec = 0;
-	timer.it_value.tv_sec = 1;
-  setitimer(ITIMER_PROF, &timer, NULL);
-
   while (1) {
     printf("foo ");
+    swapcontext(&fooctx, &scheduler);
   }
 }
 
 void bar() {
   // setting up timer
+  while (1) {
+    printf("bar ");
+    swapcontext(&barctx, &scheduler);
+  }
+}
+
+void flip() {
+  flipflop = -flipflop;
+  printf("flipflop!");
+}
+
+void schedule() {
   struct sigaction sa;
 	memset (&sa, 0, sizeof(sa));
-	sa.sa_handler = &foo;
+	sa.sa_handler = &flip;
 	sigaction (SIGPROF, &sa, NULL);
 
 	struct itimerval timer;
@@ -44,7 +46,11 @@ void bar() {
   setitimer(ITIMER_PROF, &timer, NULL);
 
   while (1) {
-    printf("bar ");
+    if (flipflop == 1) {
+      swapcontext(&scheduler, &fooctx);
+    } else {
+      swapcontext(&scheduler, &barctx);
+    }
   }
 }
 
@@ -67,9 +73,25 @@ int main() {
 	barctx.uc_stack.ss_flags=0;
   makecontext(&barctx,(void *)&bar,0);
 
-  setcontext(&foo);
+  getcontext(&scheduler);
+  void* schedulerStack = malloc(STACK_SIZE);
+  scheduler.uc_link=NULL;
+	scheduler.uc_stack.ss_sp=schedulerStack;
+	scheduler.uc_stack.ss_size=STACK_SIZE;
+	scheduler.uc_stack.ss_flags=0;
+  makecontext(&scheduler,(void *)&schedule,0);
+
+  getcontext(&flipctx);
+  void* flipStack = malloc(STACK_SIZE);
+  flipctx.uc_link=NULL;
+	flipctx.uc_stack.ss_sp=flipStack;
+	flipctx.uc_stack.ss_size=STACK_SIZE;
+	flipctx.uc_stack.ss_flags=0;
+  makecontext(&flipctx,(void *)&flip,0);
+
+  setcontext(&fooctx);
   
-  while(1);
+  // while(1);
 
   return 0;
 }
