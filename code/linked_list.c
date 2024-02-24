04c939bf86478a81da1ee035d90cea4ec71eb79a
Jack Lin <hellojjlin@gmail.com>
2024-02-24 10:58:25 -0500

    linked list library

--- code/linked_list.c
+++ code/linked_list.c
@@ -2,6 +2,16 @@
 #include "thread-worker.h"
 #include "linked_list.h"
 
-void* addToQueue(TCB* thread) {
+struct Node* createNode(struct TCB* thread) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    newNode->data = thread;
+    newNode->next = NULL;
+}
 
+void addToQueue(struct TCB* thread) {
+    
 }
--- code/linked_list.h
+++ code/linked_list.h
@@ -2,12 +2,11 @@
 #define LINKED_LIST_H
 
 #include "thread_worker_types.h"
-#include "thread-worker.h"
 
 // contains definitions for data structures needed for implementing linked list
 
 struct Node {
-    TCB* thread;
+    struct TCB* data;
     struct Node* next;
 };
 
